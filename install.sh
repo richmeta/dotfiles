c39bf83ab73243f1c364c241bf062dd0860ab9c6
Richard French <59289149+richmeta@users.noreply.github.com>
2024-01-18 13:33:57 +0000

    cleanup install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 # install shell/scripts for bash/zsh
 
 rcfile() {
-    local fn=~/.$1              # eg ~/.bashrc
+    local fn=~/$1              # eg ~/.bashrc
     local src="$PWD/$1"
     if [[ -f $fn ]]; then
         echo "moving $fn to $fn.old"
@@ -17,14 +17,43 @@ rcfile() {
     fi
 }
 
-rcfile bashrc
-rcfile bash_profile
-rcfile bash_aliases
-rcfile zshenv
-rcfile zshrc
-rcfile zprofile
+rcfile ".bashrc"
+rcfile ".bash_profile"
+rcfile ".bash_aliases"
+rcfile ".zshenv"
+rcfile ".zshrc"
+rcfile ".zprofile"
+rcfile ".tmux.conf"
+rcfile ".ripgreprc"
+rcfile ".gitignore_global"
 
-
+# tmux
 if [[ ! -d ~/.tmux/plugins/tpm ]]; then
     git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+fi
+
+# vim
+if [ ! -e ~/.vim ]; then
+    ln -sv $(readlink -f .vim) ~/.vim
+    .vim/install.sh
+fi
+
+# dirs
+if [ ! -d ~/.config ]; then
+    mkdir -p ~/.config
+fi
+
+# nvim
+if [ ! -e ~/.config/nvim ]; then
+    ln -sv $(readlink -f .config/nvim) ~/.config/nvim 
+fi
+
+# ctags
+if [ ! -e ~/.config/ctags ]; then
+    ln -sv $(readlink -f .config/ctags) ~/.config/ctags 
+fi
+
+# vifm
+if [ ! -e ~/.config/vifm ]; then
+    ln -sv $(readlink -f .config/vifm) ~/.config/vifm 
 fi
