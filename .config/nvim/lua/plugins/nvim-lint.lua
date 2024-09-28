fb487020b338240d20a416b76e85f9d3fa98f8f1
Richard French <59289149+richmeta@users.noreply.github.com>
2024-09-28 11:41:21 +0100

    nvim lint updates

--- .config/nvim/lua/plugins/nvim-lint.lua
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -9,11 +9,31 @@ local defaults = {
 }
 
 -- per project linting (overrides defaults)
---  let g:desired_linters = { "python": ["mypy", "ruff"] }
+--  let g:desired_linters = { "python": ["dmypy" | "mypy", "ruff"] }
 --  let g:desired_linters_options = { "python": {"mypy": "--config-file X"} }
+
+-- attempting to add dmypy
+local function add_dmypy(lint)
+    lint.linters.dmypy = {
+        cmd = 'dmypy',
+        stdin = false,
+        args = {"check"},
+        stream = "stdout",
+        ignore_exitcode = true,
+        ignore_errors = false,
+        parser = vim.deepcopy(lint.linters.mypy.parser)
+    }
+end
+
+local function start_dmypy() 
+    -- todo: check if running and start if not
+    -- dmypy start --log-file=dmypy.log -- --config-file pyproject.toml --show-column-numbers --show-error-end --hide-error-codes --hide-error-context --no-color-output --no-error-summary --no-pretty --python-executable=/Users/richard.french/Library/Caches/pypoetry/virtualenvs/data-rest-api-MLb99vsf-py3.12/bin/python3
+end
 
 return {
     "mfussenegger/nvim-lint",
+
+    enabled = true,
 
     event = {
         "BufReadPre",
@@ -29,6 +49,9 @@ return {
         -- only insert if executable
         for lang, linters in pairs(desired) do
             for _, linter in pairs(linters) do
+                if linter == "dmypy" then
+                    add_dmypy(lint)
+                end
                 if executable(linter) == 1 then
                     if not lint.linters_by_ft[lang] then
                         lint.linters_by_ft[lang] = {}
@@ -43,23 +66,21 @@ return {
             end
         end
 
-        -- flake8
+        local has_flake8 = util.has_value(lint.linters_by_ft.python, "flake8")
+        local has_mypy = util.has_value(lint.linters_by_ft.python, "mypy")
+        -- local has_dmypy = util.has_value(lint.linters_by_ft.python, "dmypy")
+
         if lint.linters_by_ft.python then
-            if util.has_value(lint.linters_by_ft.python, "flake8") then
+            if has_flake8 then
                 local flake8 = lint.linters.flake8
                 table.insert(flake8.args, "--ignore=E501,E704,E731,W391,F403,W292")
             end
 
-            -- mypy
-            if util.has_value(lint.linters_by_ft.python, "mypy") then
+            if has_mypy then
                 local mypy_config = file.any_exists({"mypy.ini", ".mypy.ini", "pyproject.toml"})
                 if mypy_config ~= nil then
                     local mypy = lint.linters.mypy
                     table.insert(mypy.args, "--config-file="..mypy_config)
-                    -- TODO: linting through stdin
-                    -- table.insert(mypy.args, "-")
-                    -- mypy.stdin = true
-                    -- mypy.parser = require('lint.parser').from_pattern('(%f[^:]:%d+: %m)', {'lnum', 'message'})
                 end
             end
         end
@@ -76,9 +97,15 @@ return {
 
         local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        local events = { "BufEnter", "BufWritePost", "InsertLeave" }
+        if has_mypy then
+            -- mypy is slow
+            events = {"BufWritePost"}
+        end
+
+        vim.api.nvim_create_autocmd(events, {
             group = lint_augroup,
-            callback = function()
+            callback = function(ev)
                 if vim.b.lint_enabled ~= false then
                     lint.try_lint()
                 end
